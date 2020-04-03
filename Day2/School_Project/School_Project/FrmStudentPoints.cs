8f922c8cd88c80b8ef327fb735b63bceeef3f591
Eda Çam <caam.edaa@gmail.com>
2020-04-03 04:52:31 +0300

    School Procejt Part1 with SQL Database

---
+++ Day2/School_Project/School_Project/FrmStudentPoints.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace School_Project
+{
+    public partial class FrmStudentPoints : Form
+    {
+        public FrmStudentPoints()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-IGQJ2MA;Initial Catalog=BonusOkul;Integrated Security=True");
+        public string number;
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void FrmStudentPoints_Load(object sender, EventArgs e)
+        {
+            SqlCommand komut1 = new SqlCommand("Select * From TBL_POINTS where STDID=@p1", baglanti);
+            komut1.Parameters.AddWithValue("@p1", number);
+            this.Text = number.ToString();
+            SqlDataAdapter da = new SqlDataAdapter(komut1);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
