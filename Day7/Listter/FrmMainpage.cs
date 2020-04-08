b14ccbad13a8cf9fa7fe8903da220003acacab31
Eda Çam <caam.edaa@gmail.com>
2020-04-08 23:14:21 +0300

    Lister Project SignUp form and main page

---
+++ Day7/Listter/FrmMainpage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listter
+{
+    public partial class FrmMainpage : Form
+    {
+        public FrmMainpage()
+        {
+            InitializeComponent();
+        }
+        public string name;
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FrmLogin fr = new FrmLogin();
+            fr.Show();
+        }
+
+        private void FrmMainpage_Load(object sender, EventArgs e)
+        {
+            LblUser.Text = name.ToString();
+        }
+    }
+}
---
+++ Day7/Listter/FrmSignup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Listter
+{
+    public partial class FrmSignup : Form
+    {
+        public FrmSignup()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FrmLogin fr = new FrmLogin();
+            fr.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        Connection bgl = new Connection();
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("Insert into Tbl_Users (Username,Userpassword) values (@p1,@p2)",bgl.connection());
+            command.Parameters.AddWithValue("@p1", TxtUserName.Text);
+            command.Parameters.AddWithValue("@p2", TxtPassword.Text);
+            command.ExecuteNonQuery();
+            bgl.connection().Close();
+            MessageBox.Show("Your registration has been completed","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
